353b34c9186a73defc8df5a7c7dea233468a3550
Doan Ba Huy <sonqn2@msb.com.vn>
2025-10-17 16:33:22 +0700

    Add base repository

---
+++ src/repositories/base.interface.repository.ts
@@ -0,0 +1,35 @@
+export interface IBaseRepository<T> {
+  create(data: T | any): Promise<T>;
+
+  findOneById(id: number | string): Promise<T>;
+
+  findOneByCode(code: string): Promise<T>;
+
+  findOneByCondition(filterCondition: any): Promise<T>;
+
+  findAll(): Promise<T[]>;
+
+  remove(id: string): Promise<any>;
+
+  findByIdAndUpdate(id: string, data: T | any): Promise<T>;
+
+  deleteByCondition(condition: any): Promise<any>;
+
+  deleteOneByCondition(condition: any): Promise<any>;
+
+  updateByCondition(condition: any, data: T | any): Promise<any>;
+
+  findAllByCondition(condition: any): Promise<T[] | any>;
+
+  createMany(data: T | any): Promise<any>;
+
+  softDelete(id: string, userId?: number): Promise<any>;
+
+  count(condition?: any): Promise<number>;
+
+  updateManyByCondition(condition: any, dataUpdate: any): Promise<any>;
+
+  createOrUpdate(dataUpdate: any): Promise<any>;
+
+  bulkWrite(bulkOps: any): Promise<any>;
+}
