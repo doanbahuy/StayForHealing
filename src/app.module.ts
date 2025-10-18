af0bda601557d4d60f70e771f5314fd48a2d9cac
Doan Ba Huy <huytn073@gmail.com>
2025-10-18 14:13:48 +0700

    m

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      validate: envConfig,
+      isGlobal: true,
+      cache: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
