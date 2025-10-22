c5f8229ce7e2bff465532926db5025ea130a40a6
Doan Ba Huy <sonqn2@msb.com.vn>
2025-10-22 16:58:06 +0700

    m

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,8 @@ import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
-import { ExceptionInterceptor } from '@core/interceptors/exception.interceptor';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import fastifyMultipart from 'fastify-multipart';
-import { VersioningType } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { LoggingInterceptor } from '@core/interceptors/logging.interceptor';
 
 async function bootstrap() {
   const fastifyAdapter = new FastifyAdapter({ bodyLimit: 50048576 });
@@ -26,34 +22,12 @@ async function bootstrap() {
   const configService = app.get(ConfigService);
 
   const corsOptions = {
-    origin: configService.get('CORS_ORIGIN'),
-    allowedHeaders: configService.get('CORS_ALLOWED_HEADERS'),
+    origin:'*',
+    allowedHeaders: '*',
   };
 
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
-  app.register(require('@fastify/cors'), corsOptions);
-
-  app.setGlobalPrefix(configService.get('API_PATH'));
-  const options = new DocumentBuilder()
-    .setTitle('API docs')
-    .addTag('users')
-    .addTag('tasks')
-    .addBearerAuth()
-    .setVersion('1.0')
-    .build();
-  const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('api', app, document);
-  app.enableVersioning({
-    type: VersioningType.HEADER,
-    header: 'x-api-version',
-  });
-  await app.useGlobalInterceptors(
-    new ExceptionInterceptor(),
-    app.get(LoggingInterceptor),
-  );
-
   await app.startAllMicroservices();
-  await app.listen(configService.get('SERVER_HTTP_PORT'), '0.0.0.0');
+  await app.listen(3001);
 }
 
 bootstrap();
